20438d8a2271f6dda1330dabc1686b21d0c14174
Riley Gelwicks <riley.gelwicks@gmail.com>
2015-10-17 15:32:24 -0700

    added my initial models to the app

---
+++ models/microblog.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var BlogSchema = new Schema({
+    title: {
+    	type: String,
+    	require: true
+    },
+    content: {
+    	type: String,
+    	require: true
+    },
+    date: {
+      type: Number,
+      require: true
+    }
+});
+
+var blog = mongoose.model('Blog', BlogSchema);
+module.exports = Blog;

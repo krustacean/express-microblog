cddb59864d3bae4e863d6c9f20201bdb52cdd91f
Riley Gelwicks <riley.gelwicks@gmail.com>
2015-10-17 15:22:03 -0700

    added libraries and started server also set up index route

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-var express = requires('express');
+var express = require('express');
+var app = express();
+var path = require("path");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+
+app.set('view engine', 'ejs')
+//start the server on port 8080
+app.listen(8080, function (){
+  console.log("listening on port 8080");
+});
+
+app.get('/', fucntion(request, response){
+    response.send("we are running");
+})
